e596e97a34b53203b0edbd1e6fbd4d30db3fb698
Leandro Cotrim <cotrim.leandro@gmail.com>
2022-04-04 15:35:07 -0300

    Criando dtos

---
+++ api/src/Dtos/cnab-loja-filter.model.ts
@@ -0,0 +1,6 @@
+import {Model} from '@loopback/repository';
+
+export class CnabLojaFilter extends Model {
+  IdArquivoCnab: string;
+  NomeLoja: string;
+}
---
+++ api/src/Dtos/cnab-loja-result.model.ts
@@ -0,0 +1,5 @@
+import {Model} from '@loopback/repository';
+
+export class CnabLojaFilterResult extends Model {
+
+}
