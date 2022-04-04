e5b5816c39e0066dc99597045c03a0306e8c7e9a
Leandro Cotrim <cotrim.leandro@gmail.com>
2022-04-04 14:42:11 -0300

    Configurando rota

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { CnabLojaComponent } from './components/cnab-loja/cnab-loja.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: FileUploadComponent, path: 'file-upload' },
+  { component: CnabLojaComponent, path: 'cnab-loja' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
