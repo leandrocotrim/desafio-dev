a479040589ecf280b74aa418456645e9093885cf
Leandro Cotrim <cotrim.leandro@gmail.com>
2022-04-09 23:41:58 -0300

    Serviço para upload e relatório

--- front/src/app/services/cnab-loja.service.ts
+++ front/src/app/services/cnab-loja.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cnab } from '../models/cnab';
+import { FilterCnab } from '../models/filter-cnab';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class CnabLojaService {
 
   constructor(private http: HttpClient) { }
 
-  find(findCnab: FindCnab): Observable<Cnab[]> {
+  getFilter(): Observable<FilterCnab> {
     return this.http.get(
-      'http://localhost:3000/files',
+      'http://localhost:3000/cnab-loja/filtros',
+    ) as Observable<FilterCnab>;
+  }
+
+  getRelatorio(findCnab: FindCnab): Observable<Cnab[]> {
+    return this.http.get(
+      `http://localhost:3000/cnab-loja/${findCnab.IdArquivoCnab}/${findCnab.NomeArquivo}`,
     ) as Observable<Cnab[]>;
   }
 }
